d0557a0a20e8f0d6da4f8a88bc1fdd2cb5bb5a06
toddmartin <kj4ntv@gmail.com>
2021-02-19 01:48:27 -0800

    NEW ENTRY POINT
    consolidated some code and made a block of code that was reusable. Need a back
    in to take a username and password and check to see if a client's log info is
    correct.

--- src/MemeDatabase/MemeDatabase.java
+++ src/MemeDatabase/MemeDatabase.java
@@ -4,16 +4,30 @@ import java.util.Scanner;
 
 public class MemeDatabase {
 
-    
-
     public static void main (String[] args) {
-        Scanner scan = new Scanner(System.in);
+        String username;
+        String password;
         System.out.println("Welcome to Meme Database!");
-        String input = "";
-        while (!input.equals("y") || !input.equals("n")) {
-            System.out.println("DO you have an account? [y/n] ");
-            input = scan.nextLine();
+        if (getInput("Do you have an account? [y/n] ") != null) { // if the user has an account
+            username = getInput("Username");
+            password = getInput("Password");
+        } else { // if the user does not have an account and needs to register an account
+            username = getInput("Enter a username you want");
+            password = getInput("Enter the password you want");
         }
+    }
+
+    public static getInput (String question) {
+        Scanner scan = new Scanner(System.in);
+        String input = "";
+        String confirm = "";
+        while (!confirm.equals("y") || !confirm.equals("yes")) {
+            System.out.print(question + ": ");
+            input = scan.nextLine();
+            System.out.print("Is " + input + " correct? [y/n] ");
+            confirm = scan.nextLine();
+        }
+        return input;
     }
 
 }
