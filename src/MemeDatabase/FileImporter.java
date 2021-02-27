849da263f44a8fc0b29652ecb5324ba6d37be656
toddmartin <kj4ntv@gmail.com>
2021-02-26 23:12:44 -0800

    FIXED ERRORS

--- src/MemeDatabase/FileImporter.java
+++ src/MemeDatabase/FileImporter.java
@@ -5,19 +5,20 @@ import javax.swing.JFileChooser;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.File;
 
 public class FileImporter {
 
-    public static boolean import () {
+    public static boolean importMeme (int meme_id) {
 
-        JFileChooser chooser = new JFileChooser(int meme_id);
+        JFileChooser chooser = new JFileChooser();
         try {
             int returnVal = chooser.showOpenDialog(null);
-            File file = chooser.getSelectedFile():
+            File file = chooser.getSelectedFile();
             FileInputStream fileIn = new FileInputStream(file);
             String fileName = file.getName();
             String fileNameParts[] = fileName.split(".");
-            FileOutputStream fileOut = new FileOutputStream(Integer.toString(meme_id) + "." + fileNameParts[1]);
+            FileOutputStream fileOut = new FileOutputStream("memes/"+Integer.toString(meme_id) + "." + fileNameParts[1]);
             int n;
             while ((n = fileIn.read()) != -1) {
                 fileOut.write(n);
